06de9cd98bfd9639fafe972b9547ffc5c11cbfb3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-16 14:27:45 +1200

    animations!

--- pigeon-/scripts/newplayer.gd
+++ pigeon-/scripts/newplayer.gd
@@ -1,20 +1,46 @@
 extends CharacterBody2D
 
-#REMEMBER THAT control + / IS A COMMENT
-
-var speed = 200.0
+# REMEMBER THAT control + / IS A COMMENT
+var speed = 250.0
+var is_dead = false
 var jump_speed = -375.0
 var double_jump_speed = -450.0
-#yaaay double jump works!!!!!
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 var jump_counter = 0  # Counts how many jumps performed (max 2)
+
+func death():
+	if is_dead: # Prevent multiple death calls
+		return
+
+	is_dead = true
+	$AnimatedSprite2D.play("death")
+	set_physics_process(false) # Stop character movement and physics
 
 func _physics_process(delta):
 	# Apply gravity
 	velocity.y += gravity * delta
 
+	# Get horizontal input
+	var direction = Input.get_axis("left", "right")
+	velocity.x = direction * speed
+
+	# Flip sprite based on direction
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = true
+	elif direction > 0:
+		$AnimatedSprite2D.flip_h = false
+
+
+
+	
+	if is_on_floor():
+		if abs(velocity.x) > 10:
+			$AnimatedSprite2D.play("walking")
+		else:
+			$AnimatedSprite2D.play("idle")
+	
+	if !is_on_floor():
+		$AnimatedSprite2D.play("jumping")
 	# Reset jump counter when on floor
 	if is_on_floor():
 		jump_counter = 0
@@ -22,15 +48,15 @@ func _physics_process(delta):
 	# Handle jump input
 	if Input.is_action_just_pressed("jump") and jump_counter < 2:
 		if jump_counter == 0:
-			# First jump
-			velocity.y = jump_speed
+			velocity.y = jump_speed  # First jump
 		else:
-			# Second jump (double jump)
-			velocity.y = double_jump_speed
+			velocity.y = double_jump_speed  # Second jump
 		jump_counter += 1
 
-	# Get horizontal input
-	var direction = Input.get_axis("left", "right")
-	velocity.x = direction * speed
-
+	# Move and slide with UP as the floor direction
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group('spikes'):
+		death()
--- pigeon-/scripts/player.gd
+++ pigeon-/scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 200.0
+var speed = 250.0
 var jump_speed = -375.0
 var can_jump = true
 
@@ -22,6 +22,14 @@ func _physics_process(delta):
 	# Add the gravity.
 	velocity.y += gravity * delta
 
+	var direction = Input.get_axis("left", "right")
+	velocity.x = direction * speed
+
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = true
+	elif direction > 0:
+		$AnimatedSprite2D.flip_h = false
+
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and can_jump:
 		jump()
@@ -29,8 +37,15 @@ func _physics_process(delta):
 	if (is_on_floor() == false) and can_jump and $CoyoteTimer.is_stopped():
 		$CoyoteTimer.start()
 	
+	
+	if is_on_floor():
+		if abs(velocity.x) > 10:
+			$AnimatedSprite2D.play("walking")
+		else:
+			$AnimatedSprite2D.play("idle")
+	
+	if !is_on_floor():
+		$AnimatedSprite2D.play("jumping")
 	# Get the input direction.
-	var direction = Input.get_axis("left", "right")
-	velocity.x = direction * speed
 
 	move_and_slide()
